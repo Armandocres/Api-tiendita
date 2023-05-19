f2bd19109612feb88fb4ef347043b9d0cfa83263
Armandocres <armandocres99@gmail.com>
2023-05-19 16:35:02 -0600

    rutas

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ app.get('/', (req, res) => {
   res.send('hello world');
 })
 
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Producto1',
+    price: 1000,
+  })
+})
+
+app.get('/categories', (req, res) => {
+  res.json({
+    id: 1,
+    category: 'ropa',
+  })
+})
+
 app.listen(port, () => {
   console.log('estoy corriendo en', port);
 });
